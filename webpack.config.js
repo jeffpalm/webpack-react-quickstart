41e47cd240c44e19302b4aca3cce70e6f5dd649c
Jeff Palmer <jeff@palmytree.com>
2020-10-05 20:59:38 -0500

    updated configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,41 @@
 const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
   devServer: {
     contentBase: path.resolve(__dirname, './src'),
+    contentBasePublicPath: path.resolve(__dirname, './public'),
     historyApiFallback: true
   },
   entry: path.resolve(__dirname, './src/index.js'),
   module: {
     rules: [
       {
-        test: /\.js$/,
-        use: 'babel-loader'
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader'
+          }
+        ]
       }
     ]
   },
   output: {
     filename: 'bundle.js'
-  }
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './public/index.html',
+      filename: './index.html'
+    }),
+    new ESLintPlugin()
+  ]
 };
