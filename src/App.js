9eef799e6e68447ffcdfe27d95aa1811ea8f1fc3
Jeff Palmer <jeff@palmytree.com>
2020-10-07 17:31:40 -0500

    added css/sass loaders + aliases

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'CSS/reset.css';
+import 'SCSS/App.scss';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <h1>Webpack/React QuickStart Template</h1>
+    </div>
+  );
+};
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,16 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
+  resolve: {
+    alias: {
+      Config: path.resolve(__dirname, 'src', 'config'),
+      Components: path.resolve(__dirname, 'src', 'components'),
+      Assets: path.resolve(__dirname, 'src', 'assets'),
+      App: path.join(__dirname, 'src'),
+      CSS: path.join(__dirname, 'src', 'assets', 'css'),
+      SCSS: path.join(__dirname, 'src', 'assets', 'scss')
+    }
+  },
   devServer: {
     contentBase: path.resolve(__dirname, './src'),
     contentBasePublicPath: path.resolve(__dirname, './public'),
@@ -23,6 +33,26 @@ module.exports = {
         use: [
           {
             loader: 'html-loader'
+          }
+        ]
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('node-sass'),
+              sassOptions: {
+                fiber: require('fibers')
+              }
+            }
           }
         ]
       }
